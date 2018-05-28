49fee331c0fd1e6c607027b72f440a0e62deb33e
Artur Stepovyi <astepovy@e1r7p16.unit.ua>
2018-05-28 16:42:45 +0300

    arthur pidor

--- hidden_file/Ressources/get_flag.sh
+++ hidden_file/Ressources/get_flag.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -e robots=off -r -np -m --reject "*robots.txt*" "http://10.0.2.15/.hidden/"
-find 10.0.2.15/.hidden/ -name "README" -exec cat "{}" >> test_file \;
+wget -e robots=off -r -np -m --reject "*robots.txt*" "http://$IP/.hidden/"
+find $IP/.hidden/ -name "README" -exec cat "{}" >> test_file \;
 cat test_file | awk "{print $1}" | sort -u
